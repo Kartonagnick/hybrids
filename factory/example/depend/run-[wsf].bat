2454d2122bae0cae1ceb14c743c4a0d9e51b77a1
Kartonagnick <Kartonagnick@gmail.com>
2024-02-14 03:02:30 +0300

    add: factory/example/depend/run-[wsf].bat (001) PRE

---
+++ factory/example/depend/run-[wsf].bat
@@ -0,0 +1,42 @@
+@echo off & call :checkParent || exit /b
+
+rem ============================================================================
+rem ============================================================================
+
+rem --- local/hybrids                             [example/depend/run-[wsf].bat]
+rem [2024-02-15][09:00:00] 001 Kartonagnick PRE
+
+rem ============================================================================
+rem ============================================================================
+
+:main
+  setlocal
+  echo [RUN-EXAMPLE.WSF] run... v0.0.1    
+  cscript "/Nologo" "%~dp0depend.wsf" || goto :failed
+:success
+  echo [RUN-EXAMPLE.WSF] completed successfully
+  exit /b 0
+:failed
+  echo [RUN-EXAMPLE.WSF] finished with erros
+exit /b 1
+
+rem ============================================================================
+rem ============================================================================
+
+:normalizeD
+  set "%~1=%~dpfn2"
+exit /b
+
+:setOwnerD
+  if defined eDIR_OWNER (exit /b)
+  cls & echo. & echo.
+  call :normalizeD eDIR_OWNER "%~dp0."
+exit /b
+
+:checkParent
+  if errorlevel 1 (echo [ERROR] was broken at launch & exit /b 1)
+  call :setOwnerD
+exit /b
+
+rem ============================================================================
+rem ============================================================================
